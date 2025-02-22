4ea2d4653704fe7ec9181ee870658825a7e4e7a9
Ayush Nayak <nayaksubham18@gmail.com>
2025-02-23 00:54:14 +0530

    fix: added frontend

--- backend/services/blockchainService.js
+++ backend/services/blockchainService.js
@@ -1,6 +1,6 @@
 import { ethers } from "ethers";
 import config from "../config/config.js";
-import MemeNFTAbi from "../blockchain/contracts/MemeNFT.json" assert { type: "json" };
+import MemeNFTAbi from "../../blockchain/contracts/MemeNFT.json" assert { type: "json" };
 
 const provider = new ethers.JsonRpcProvider(config.blockchain.rpcUrl);
 const wallet = new ethers.Wallet(config.blockchain.privateKey, provider);
